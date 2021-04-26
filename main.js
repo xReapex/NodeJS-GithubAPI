e63470d155a3ab9e585086937c08e247dd243dd9
xReapex <tr7296228@gmail.com>
2021-04-26 18:41:19 +0200

    :hammer: Refactor file tree

--- main.js
+++ main.js
@@ -1,57 +1,22 @@
-//Import 
+/* Import module */
 const { request } = require("@octokit/request");
 
-class GithubAPI {
+/* Import Functions */
+const { Functions } = require("./src/functions")
+
+class GithubAPI extends Functions {
 
     /* Constructor */
-    constructor(API_KEY) {
+    super(API_KEY) {
         this.API_KEY = API_KEY;
     }
 
-    /********************************/
-    /*           Functions          */
-    /********************************/
-
+    /* Global Authentication */
     requestWithAuth = request.defaults({
         headers: {
             authorization: this.API_KEY,
         },
     });
-
-    async getUser(username) {
-        let result = await this.requestWithAuth("GET /users/{username}", {
-            headers: {
-                authorization: this.API_KEY,
-            },
-            username: username
-        });
-        return result.data;
-    }
-
-    async getFollowers(username) {
-            let result = await request("GET /users/{username}/followers", {
-                headers: {
-                    authorization: this.API_KEY,
-                },
-                username: username,
-            });
-            return result.data;
-    }
-
-    async isFollowedBy(follower, target) {
-        try {
-            await request("GET /users/{username}/following/{target_user}", {
-                headers: {
-                    authorization: this.API_KEY,
-                },
-                username: follower,
-                target_user: target
-            });
-            return true;
-        } catch (error) {
-            return false;
-        }
-    }
 }
 
 /* Export Class */
---
+++ src/functions.js
@@ -0,0 +1,32 @@
+class Functions
+{
+    async getUser(username) {
+        let result = await this.requestWithAuth("GET /users/{username}", {
+            username: username
+        });
+        return result.data;
+    }
+
+    async getFollowers(username) {
+            let result = await this.requestWithAuth("GET /users/{username}/followers", {
+                username: username,
+            });
+            return result.data;
+    }
+
+    async isFollowedBy(follower, target) {
+        try {
+            await this.requestWithAuth("GET /users/{username}/following/{target_user}", {
+                username: follower,
+                target_user: target
+            });
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+}
+
+module.exports = {
+    Functions
+}
