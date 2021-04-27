0e1f6cb0438f185eecc6271f3013328f3214ba60
xReapex <tr7296228@gmail.com>
2021-04-27 11:52:09 +0200

    :sparkles: Adding search repository function

--- src/functions.js
+++ src/functions.js
@@ -1,19 +1,18 @@
-class Functions
-{
+class Functions {
+
+    /* USERS */
     async getUser(username) {
         let result = await this.requestWithAuth("GET /users/{username}", {
             username: username
         });
         return result.data;
     }
-
     async getFollowers(username) {
-            let result = await this.requestWithAuth("GET /users/{username}/followers", {
-                username: username,
-            });
-            return result.data;
+        let result = await this.requestWithAuth("GET /users/{username}/followers", {
+            username: username,
+        });
+        return result.data;
     }
-
     async isFollowedBy(follower, target) {
         try {
             await this.requestWithAuth("GET /users/{username}/following/{target_user}", {
@@ -25,11 +24,22 @@ class Functions
             return false;
         }
     }
-
     async getFollowing(username) {
         let result = await this.requestWithAuth("GET /users/{username}/following", {
             username: username,
         });
+        return result.data;
+    }
+
+    /* SEARCH */
+    async searchRepositories(name, per_page, page, sort = 'best-match') {
+
+        let result = await this.requestWithAuth('GET /search/repositories', {
+            q: name,
+            sort: sort,
+            per_page: per_page,
+            page: page
+          })
         return result.data;
     }
 }
